f910d7b301e762ed37e3a9aba654be05bddebb91
dwkim <rhoowd@gmail.com>
2016-10-08 21:03:24 +0900

    hi

--- service/dst.sh
+++ service/dst.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 echo "dst"
+vlc -vvv rtsp://10.10.0.2:8554/test
--- service/src.sh
+++ service/src.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 echo "src"
+vlc -vvv ~/Desktop/720p.mp4 --sout '#transcode{vcodec=h264,acodec=mpga,ab=128,channels=2,samplerate=11025}:rtp{sdp=rtsp://:8554/test}'
